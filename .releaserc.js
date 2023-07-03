41b7e685ce9134e4e9f5d9cde0c3cc5fad488ee5
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-07-03 12:21:39 +0530

    fix(root): initial

--- .releaserc.js
+++ .releaserc.js
@@ -1,18 +1,15 @@
 module.exports = {
-  branches: ['master'],
-  tagFormat: '${name}-v${version}',
+  tagFormat: '${APP_NAME}-v${version}',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', { changelogTitle: '# CHANGELOG' }],
-    '@semantic-release/npm',
-    '@semantic-release/github',
     [
-      '@semantic-release/git',
+      '@semantic-release/changelog',
       {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        changelogTitle: '# CHANGELOG',
       },
     ],
+    '@semantic-release/github',
+    '@semantic-release/git',
   ],
 };
