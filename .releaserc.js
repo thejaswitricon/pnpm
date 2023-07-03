74519c14787f97e339956fc418cade6038ce6bb4
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-07-03 11:54:11 +0530

    fix(root): initial

--- .releaserc.js
+++ .releaserc.js
@@ -1,21 +1,18 @@
 module.exports = {
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      [
-        '@semantic-release/changelog',
-        {
-          changelogTitle: '# CHANGELOG',
-          changelogFile: 'modules/aws/ecs/ecs-simple/modules/CHANGELOG.md'
-        }
-      ],
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['modules/aws/ecs/ecs-simple/modules/CHANGELOG.md']
-        }
-      ]
-    ]
-  };
-  
+  branches: ['master'],
+  tagFormat: '${name}-v${version}',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogTitle: '# CHANGELOG' }],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
